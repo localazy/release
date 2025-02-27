a7746c474fc20ae3f52ed1b90c223949bdef7727
Honza Bubeník <honza.bubenik@localazy.com>
2025-02-27 02:37:31 +0100

    🔧 chore: changes 3

--- ci/release-management/src/main.ts
+++ ci/release-management/src/main.ts
@@ -1,4 +1,3 @@
-// import 'source-map-support/register';
 import { handleException } from '@/functions/utils/error/handle-exception';
 import { versionManagementAction } from '@/version-management-action';
 
